9b83091a4704fa7f7fca745b77ed908c83dcfba2
shady786 <sachmat786@gmail.com>
2015-09-01 07:48:00 +0200

    More cleanup and git ignore

--- packages/1.0/js/entityList.js
+++ packages/1.0/js/entityList.js
@@ -7,24 +7,14 @@ _entityList = {
     },
 
     onLoaded: function () { var _ = this;
-
-
         _model.getAll("callCycles", function (entities) {
-            _entityList.model = entities;
-
-            //alert("_entityList.model" + JSON.stringify(_entityList.model));
-
-            if (_entityList.model.length > 0) {
-                //alert("entities" + JSON.stringify(_entityList.model[0]));
-
-                layout.attach('#entityFront');
+            if (entities) {
+                _entityList.model = entities;
+            } else {
+                _entityList.model = [];
             }
-
+            layout.attach('#entityFront');
         });
-
-
-
-
     },
 
     onMessage : function() {
@@ -32,11 +22,6 @@ _entityList = {
     },
 
     entityCtrl: function($scope) {
-
-        //alert("inside entityCtrl!!!");
-
         $scope.entities    = _entityList.model;
-
-
     }
 };;;
