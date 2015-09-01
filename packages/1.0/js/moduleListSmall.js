b018b41e54a29a45631ced108dcd2996e9390efe
shady786 <sachmat786@gmail.com>
2015-09-01 11:44:53 +0200

    Flipping to questionaire on phone and updating right layout on tablet

--- packages/1.0/js/moduleListSmall.js
+++ packages/1.0/js/moduleListSmall.js
@@ -2,6 +2,7 @@ _moduleListSmall = {
 
     model: null,
     entityId : null,
+    itemsModel : "Nothing to Display",
 
     onExit : function() { var _ = this;
 
@@ -25,5 +26,42 @@ _moduleListSmall = {
 
     moduleCtrl: function($scope) {
         $scope.modules    = _moduleListSmall.model;
+    },
+    moduleViewerCtrl : function($scope) {
+        $scope.itemsHTML = _moduleListSmall.itemsModel;
+    },
+    loadModuleView : function(data) {
+        var id = $(data).attr("module_id");
+        _moduleListSmall.flip("moduleListSmallViewer",
+            function (cb) {
+                _moduleListSmall.itemsModel = "Some bullshit!";
+                _log.d("FLIPPING TO : ");
+                cb();
+            },
+            function () {
+                _log.d("FLIPPED TO : ");
+            }
+        );
+    },
+    flip : function(flipTarget, before, cb) {
+        if(before) {
+            before(function () {
+                _cardEngine.flip("moduleListSmall", flipTarget, function(release) {
+                    release();
+                    layout.attach('#'+flipTarget);
+                    if(cb) {
+                        cb();
+                    }
+                });
+            });
+        } else {
+            _cardEngine.flip("moduleListSmall", flipTarget, function(release) {
+                release();
+                layout.attach('#'+flipTarget);
+                if(cb) {
+                    cb();
+                }
+            });
+        }
     }
 };;;
