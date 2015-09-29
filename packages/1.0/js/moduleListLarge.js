fe68d1cd3e452a2ae2467a15ae8e7e9f63184774
benneb2 <benneb2@gmail.com>
2015-09-29 14:19:23 +0200

    adv tab view v0.1

--- packages/1.0/js/moduleListLarge.js
+++ packages/1.0/js/moduleListLarge.js
@@ -2,17 +2,17 @@ _moduleListLarge = {
 
     model: null,
     entityId : null,
-
+    moduleId:null,
     onExit : function() { var _ = this;
 
     },
 
     onLoaded: function () { var _ = this;
 
+
     },
 
     onMessage : function(data) {
-
 
         _moduleListLarge.entityId = data.entityId;
         _model.get("Modules", { "entityId" : _moduleListLarge.entityId }, function (moduleData) {
@@ -21,14 +21,77 @@ _moduleListLarge = {
             } else {
                 _moduleListLarge.model = [];
             }
-            layout.attach('#moduleListLargeFront');
+               _model.getAll("callCycles", function (entities) {
+                  if (entities) {
+                      _moduleListLarge.callCycles = entities;
+                  } else {
+                      _moduleListLarge.callCycles = [];
+                  }
+                  layout.attach('#moduleListLargeFront');
+                  layout.attach('#breadcrumbs');
+              });
         });
-    },
 
+
+    },
+    breadcrumbsCtrl: function($scope){
+        $scope.entity = _moduleListLarge.entityId;
+        for(var i in _moduleListLarge.callCycles)
+        {
+            if(_moduleListLarge.callCycles[i].entityId == _moduleListLarge.entityId)
+            {
+                $scope.entity = _moduleListLarge.callCycles[i].entityName;
+                break;
+            }
+        }
+
+        $scope.module = _moduleListLarge.moduleId;
+        for(var i in _moduleListLarge.model)
+        {
+            if(_moduleListLarge.model[i].id ==  _moduleListLarge.moduleId)
+            {
+                $scope.module = _moduleListLarge.model[i].name;
+                break;
+            }
+        }
+
+        $scope.record = '';
+        $scope.categories = [];
+        $scope.item = '';
+    }
+    ,
     moduleCtrl: function($scope) {
         $scope.modules    = _moduleListLarge.model;
     },
     loadModuleView : function(data) {
+
+        _moduleListLarge.moduleId = $(data).attr("module_id");
+        _cardEngine.flip("moduleListLarge","breadcrumbs");
         layout.sendMessage("moduleViewerLarge", { id : $(data).attr("module_id") });
+    }
+    ,
+    updateBreadcrumb:function(data)
+    {
+        e = document.getElementById('breadcrumbs__FACE');
+        scope = angular.element(e).scope();
+        scope.$apply(function() 
+        {  
+            scope.record = data.record;
+            scope.categories = data.categories;
+
+
+            scope.items = data.items;
+            for(var i in scope.items)
+            {
+                if(scope.items[i].name == data.item)
+                {
+                    scope.items[i].selected = true;
+                }else
+                {
+                    scope.items[i].selected = false;
+                }
+            }
+            
+        }); 
     }
 };;;
